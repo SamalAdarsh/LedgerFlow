b3c26187983a54daff137da1eabbee870800a8ba
Adarsh <codewithadarsh007@gmail.com>
2026-07-06 16:17:01 +0530

    Getting started

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async ()=>{
+    try{
+         await mongoose.connect(process.env.MONGO_URI);
+         console.log("DB connected successfully");
+
+    }catch(err){
+        console.log("Error connecting to DB:",err);
+        process.exit(1);
+
+    }
+
+}
+
+module.exports = connectDB;
