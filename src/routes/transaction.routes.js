81cf602c98ef2bc44ad2d2d4f6c3a5c6cdf1f9e7
Adarsh <codewithadarsh007@gmail.com>
2026-07-10 18:26:17 +0530

    created transaction routes

---
+++ src/routes/transaction.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth.middleware');
+const transactionController = require('../controllers/transaction.controller');
+
+const router = exress.Router();
+
+/**
+ *  - POST /api/transactions/
+ *  - Create a new transaction
+ */
+
+router.post('/',authMiddleware.authMiddleware,transactionController.createTransaction)
+
+
+/**
+ *  - POST /api/transactions/system/initial-funds
+ *  - Create initial funds transaction from system user
+ */
+
+router.post('/system/initial-funds', authMiddleware.authSystemMiddleware, transactionController.createInitialFundsTransaction)
+
+module.exports = router;
